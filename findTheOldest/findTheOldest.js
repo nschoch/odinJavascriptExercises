1ae72a7e1a08a4ff4250e9c3aefd1bd411895245
Nick Schoch <nschoch@gmail.com>
2019-04-19 05:31:01 +0000

    findTheOldest update

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-let findTheOldest = function() {
+let ageCalc = (birthYear, deathYear) => {
+    if (!deathYear) {
+        deathYear = (new Date).getFullYear();
+    }
+    return deathYear*1 - birthYear*1
+}
 
+let findTheOldest = (arrayOfPeeps) => {
+    let oldest = {};
+    arrayOfPeeps.forEach((person) => {
+        let age = ageCalc(person.yearOfBirth, person.yearOfDeath);
+        let oldestAge = ageCalc(oldest.yearOfBirth, oldest.yearOfDeath);
+        if (age > oldestAge || !oldestAge) {
+            oldest = person;
+        }
+    })
+
+    return oldest
 }
 
 module.exports = findTheOldest
